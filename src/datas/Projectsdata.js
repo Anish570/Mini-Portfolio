3ce80cc373aee19c737b9757c093095142c0a4c8
Anish Gautam <anishgautam2024@gmail.com>
2024-03-20 19:40:15 +0545

    new project details added

--- src/datas/Projectsdata.js
+++ src/datas/Projectsdata.js
@@ -9,6 +9,11 @@ const Projectsdata = [
         preview: "https://github.com/Anish570/Mini-Portfolio/blob/main/src/assets/weatherapp.jpg?raw=true",
         title:'Weather App',
         link: "http://npweather.free.nf/?i=1"
+    },
+    {
+        preview: "https://github.com/Anish570/Mini-Portfolio/blob/main/src/assets/minitodo.jpg?raw=true",
+        title:'Mini-Todos',
+        link: "https://mini-todos.vercel.app/"
     }
 ]
 
